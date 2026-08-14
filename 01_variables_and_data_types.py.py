ee76978aca23c60a80fd2bd3e01703d9e26dda66
Bohdan Bilokur <bohdanbilokur7@gmail.com>
2026-08-14 16:33:20 +0100

    Add variables and data types practice

---
+++ 01_variables_and_data_types.py.py
@@ -0,0 +1,27 @@
+#int
+number = 5
+
+#float
+fnumber = 5.7
+
+#str
+name = 'Bohdan'
+
+#bool
+status = True
+
+# Экранирование
+print('He is a "bad" person')
+
+#Перенос строки
+print('He is \na bad person')
+
+#Конкатенация
+print('Привет меня зовут ' + name)
+
+age = 17
+print('Мне ' + str(age) + ' лет')
+
+a = 5.55
+
+print(round(a))
