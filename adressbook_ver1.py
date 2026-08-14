00388a59caae36ba31022a320df75c5151cd9d3b
Bohdan Bilokur <bohdanbilokur7@gmail.com>
2026-08-14 16:43:31 +0100

    Update adressbook_ver1.py

--- adressbook_ver1.py
+++ adressbook_ver1.py
@@ -10,13 +10,13 @@ while True:
 		print('Too short')
 
 	if something == 'Artur':
-		print('Artur Bilokur, number: 095 128 8991, age: 21')
+		print('Artur Bilokur, number: 0000000, age: 21')
 	else:
 		if something == 'Igor':
-			print('Igor Bilokur, number: 096 704 4301, age: 40')
+			print('Igor Bilokur, number: 0000000, age: 40')
 		else:
 			if something == 'Liza':
-				print('Liza Bilokur, number: 068 750 6229, age: 12')
+				print('Liza Bilokur, number: 0000000, age: 12')
 			else:
 				print('Not found')
 
