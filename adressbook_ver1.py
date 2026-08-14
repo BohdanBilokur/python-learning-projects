bf48ca0c3a59f410089b5e1abb403d19c4f08c64
Bohdan Bilokur <bohdanbilokur7@gmail.com>
2026-08-14 16:37:56 +0100

    Add address book V1

---
+++ adressbook_ver1.py
@@ -0,0 +1,24 @@
+#V1
+
+name = 'Artur'   #Использую сamel case переменную
+name = 'Igor'
+running = True
+
+while True:
+	something = input('Input name:')
+	if len(something) < 2:
+		print('Too short')
+
+	if something == 'Artur':
+		print('Artur Bilokur, number: 095 128 8991, age: 21')
+	else:
+		if something == 'Igor':
+			print('Igor Bilokur, number: 096 704 4301, age: 40')
+		else:
+			if something == 'Liza':
+				print('Liza Bilokur, number: 068 750 6229, age: 12')
+			else:
+				print('Not found')
+
+	if something =='exit':
+		break
