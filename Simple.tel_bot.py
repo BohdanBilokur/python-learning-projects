15a12c07b16e050cc219f00e5be407476857005f
Bohdan Bilokur <bohdanbilokur7@gmail.com>
2026-08-14 17:03:44 +0100

    telegram_echo_bot.py

---
+++ Simple.tel_bot.py
@@ -0,0 +1,11 @@
+import telebot
+
+bot = telebot.TeleBot("Your_Bot_Token") # You can set parse_mode by default. HTML or MARKDOWN
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	bot.send_message(message.chat.id, message.text)
+
+
+bot.infinity_polling()
+
